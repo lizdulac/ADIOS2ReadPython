be49a21c881f8c571b53bdc3f7314a0f71a92e25
lizdulac <lizadulac@gmail.com>
2024-08-29 18:39:45 -0400

    Print subset of data if variable is specified

--- bpReadEx.py
+++ bpReadEx.py
@@ -2,6 +2,9 @@ import numpy
 import sys
 from adios2 import Stream, FileReader
 
+# What subset of data to print if a variable is specified
+num_read = 3
+
 var_specified = False
 if len(sys.argv) > 1:
     var_specified = True
@@ -18,10 +21,25 @@ with Stream("fs.bp", "rra") as ibpFile:
                 print(f"Variable {var_name} is SingleValue with value {inqrd_var}")
             elif inqrd_varInfo["Shape"] != "":
                 # Global Array
-                shape = inqrd_varInfo["Shape"]
+                var = ibpFile.inquire_variable(var_name)
+                data = ibpFile.read(var)
+                shape = data.shape
                 print(f"Reading global var {var_name} with shape {shape}")
-                #for i in range(0, inqrd_var.shape[1]):
-                #    print(str(inqrd_var[i]))
+                # Access Data from 1D Global Array
+                if len(shape) == 1:
+                    print(f" Reading from index 0 to {num_read}:", end=" ")
+                    for i in range(0, num_read):
+                        print(str(inqrd_var[i]), end=" ")
+                    print(f"...")
+                # Access Data from 2D Global Array
+                elif len(shape) == 2:
+                    print(f" Reading from", end="")
+                    for i in range(0, num_read):
+                        print(f" index ({i}, 0) to ({i}, {num_read}):", end=" ")
+                        for j in range(0, num_read):
+                            print(str(inqrd_var[i][j]), end=" ")
+                        print(f"...")
+                    print(f" index ({num_read}, 0) ...")
             else:
                 # Local Array
                 var = ibpFile.inquire_variable(var_name)
@@ -32,7 +50,24 @@ with Stream("fs.bp", "rra") as ibpFile:
                         var.set_step_selection([step, 1])
                         var.set_block_selection(block_id)
                         data = ibpFile.read(var)
-                        print(f"step {step} block {block_id} shape {data.shape}")
+                        shape = data.shape
+                        print(f"step {step} block {block_id} shape {shape}")
+                        # Access Data from 1D Global Array
+                        if len(shape) == 1:
+                            print(f" Reading from index 0 to {num_read}:", end=" ")
+                            for i in range(0, num_read):
+                                print(str(data[i]), end=" ")
+                            print(f"...")
+                        # Access Data from 2D Global Array
+                        elif len(shape) == 2:
+                            print(f" Reading from", end="")
+                            for i in range(0, num_read):
+                                print(f" index ({i}, 0) to ({i}, {num_read}):", end=" ")
+                                for j in range(0, num_read):
+                                    print(str(data[i][j]), end=" ")
+                                print(f"...")
+                            print(f" index ({num_read}, 0) ...")
+
     else:
         for varName in varInfo:
             type = varInfo[varName]["Type"]

53dea6804808917575cf865e1e55a30bff0308aa
lizdulac <lizadulac@gmail.com>
2024-08-29 17:15:33 -0400

    Create bp read example

---
+++ README.md
@@ -0,0 +1,1 @@
+# ADIOS2ReadPython
---
+++ bpReadEx.py
@@ -0,0 +1,49 @@
+import numpy
+import sys
+from adios2 import Stream, FileReader
+
+var_specified = False
+if len(sys.argv) > 1:
+    var_specified = True
+    var_name = sys.argv[1]
+
+with Stream("fs.bp", "rra") as ibpFile:
+    varInfo = ibpFile.available_variables()
+    if var_specified:
+        inqrd_varInfo = varInfo[var_name]
+        if inqrd_varInfo is not None:
+            inqrd_var = ibpFile.read(var_name)
+            if inqrd_varInfo["SingleValue"] == "true":
+                # Scalar Value
+                print(f"Variable {var_name} is SingleValue with value {inqrd_var}")
+            elif inqrd_varInfo["Shape"] != "":
+                # Global Array
+                shape = inqrd_varInfo["Shape"]
+                print(f"Reading var {var_name} with shape {shape}")
+                #for i in range(0, inqrd_var.shape[1]):
+                #    print(str(inqrd_var[i]))
+            else:
+                # Local Array
+                var = ibpFile.inquire_variable(var_name)
+                blocks_info = ibpFile.all_blocks_info(var_name)
+                print(f"Reading local var {var_name}") 
+                for step in range(var.steps()):
+                    for block_id in range(len(blocks_info[step])):
+                        var.set_step_selection([step, 1])
+                        var.set_block_selection(block_id)
+                        data = ibpFile.read(var)
+                        print(f"step {step} block {block_id} shape {data.shape}")
+    else:
+        for varName in varInfo:
+            print(f"Reading variable {varName}:")
+            print(f"{varInfo[varName]}:\n")
+            type = varInfo[varName]["Type"]
+            print(f"  type:  {type}\n")
+            if not varInfo[varName]["SingleValue"]:
+                shape = varInfo[varName]["Shape"]
+                print(f"  shape:  {shape}\n")
+            if varInfo[varName]["Type"] != "string":
+                min = varInfo[varName]["Min"]
+                max = varInfo[varName]["Max"]
+                print(f"  min/max:  {min} / {max}\n")
+                
